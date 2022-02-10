b3e4b0ebf9c5242a6e1722c70b0188e5b37b8a68
devnoman01 <devnoman01@gmail.com>
2022-02-10 14:19:36 +0600

    new page created and redirect check complete

---
+++ bank.js
@@ -0,0 +1,12 @@
+document.getElementById('login-button').addEventListener('click', function() {
+    
+    // collecting user email
+    const userEmail = document.getElementById('user-email').value;
+    // collecting user password
+    const userPassword = document.getElementById('user-password').value;
+    
+    // validating input
+    if(userEmail == 'admin@gmail.com' && userPassword == '1234'){
+        window.location.href = 'banking.html';
+    }
+})

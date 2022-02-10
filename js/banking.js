37c3defc66ef89c3f8d13e1aaae220fe2a72a765
devnoman01 <devnoman01@gmail.com>
2022-02-10 15:53:31 +0600

    withdraw function complete

--- js/banking.js
+++ js/banking.js
@@ -22,3 +22,28 @@ document.getElementById('deposite-submit').addEventListener('click', function(){
     // clear deposite input field
     depositeInput.value = '';
 })
+
+// handle withdraw button event
+document.getElementById('withdraw-submit').addEventListener('click', function(){
+    //getting withdraw amount
+    const withdrawInput = document.getElementById('withdraw-amount');
+    const newWithdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+
+    // updating withdraw amount
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousDeposite = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousDeposite + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update current balance
+    const currentBalanceAmount = document.getElementById('current-balance');
+    const currentBalanceText = currentBalanceAmount.innerText;
+    const previousCurrentBalance = parseFloat(currentBalanceText);
+    const newCurrentBalance = previousCurrentBalance - newWithdrawAmount;
+    currentBalanceAmount.innerText = newCurrentBalance;
+
+    // clear deposite input field
+    withdrawInput.value = '';
+})
